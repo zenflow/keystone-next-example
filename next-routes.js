1b151487173ec9c891a38342498f76d60dd086f8
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-08-30 22:09:49 -0300

    Make path for home route explicit

--- next-routes.js
+++ next-routes.js
@@ -2,6 +2,6 @@ const nextRoutes = require('next-routes')
 
 module.exports = nextRoutes()
   .add('index', '/')
-  .add('home')
+  .add('home', '/home')
   .add('library', '/:library(desenhos|fotografias|pinturas)')
   .add('gallery', '/:library(desenhos|fotografias|pinturas)/:gallery')
